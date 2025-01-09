9a4f092e7c215cef731d41b6ee1c4b97161bc6e1
Galleria-Art <evanlindsay06@gmail.com>
2025-01-09 23:46:16 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 3000,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 3000,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
